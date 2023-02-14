225ac80a2ccfdd7d26a9dbdf9653ab54c2d9da4a
alabeeb <alabeeb81@gmail.com>
2023-02-14 19:05:46 +0200

    Initial Commit

---
+++ GamingModule/GamingModule.h
@@ -0,0 +1,18 @@
+//
+//  GamingModule.h
+//  GamingModule
+//
+//  Created by Ahmad Labeeb on 14/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GamingModule.
+FOUNDATION_EXPORT double GamingModuleVersionNumber;
+
+//! Project version string for GamingModule.
+FOUNDATION_EXPORT const unsigned char GamingModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GamingModule/PublicHeader.h>
+
+
